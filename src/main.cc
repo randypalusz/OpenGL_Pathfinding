90da11d62d1ef618400964568b69d61cdd597a05
randypalusz <ibanezguy998@gmail.com>
2020-07-23 14:09:57 -0400

    render teal window, destroy on pressing 'END'

--- src/main.cc
+++ src/main.cc
@@ -12,26 +12,45 @@
 #include "A_Star.hpp"
 
 void glfunctions();
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_END) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
 
 void glfunctions() {
-  bool running = true;
   glfwInit();
   glfwWindowHint(GLFW_SAMPLES, 4);
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
   GLFWwindow* window = glfwCreateWindow(800, 600, "Window", NULL, NULL);
   if (!window) {
+    std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
   }
+
   glfwMakeContextCurrent(window);
-  while (running) {
-    glfwPollEvents();
-    if (glfwGetKey(window, GLFW_KEY_END) == GLFW_PRESS) {
-      glfwSetWindowShouldClose(window, true);
-      running = false;
-    }
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
   }
-  glfwDestroyWindow(window);
+  glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+    glfwSwapBuffers(window);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwPollEvents();
+  }
+  glfwTerminate();
 }
 
 int main(int argc, char** argv) {
