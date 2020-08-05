2e84508278c283be172ed05fb020ca4831d5d2f2
randypalusz <ibanezguy998@gmail.com>
2020-08-05 15:00:53 -0400

    added SFML to CMakeLists.txt, example code in main.cc

--- src/main.cc
+++ src/main.cc
@@ -3,10 +3,29 @@
 // https://github.com/JoeyDeVries/LearnOpenGL
 // https://learnopengl.com/
 
+#include <SFML/Graphics.hpp>
+
 #include <chrono>
 #include <vector>
 
 #include "A_Star.hpp"
+
+void sfmlTest() {
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Window");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
+}
 
 int main(int argc, char** argv) {
   // std::vector<std::vector<char>> grid{};
@@ -24,6 +43,7 @@ int main(int argc, char** argv) {
   //     std::vector<char>{'.', '#', '.', '.', '.', '.', '#', '.', '.', '.', '.', '.'});
 
   // A_Star path{grid};
+  sfmlTest();
   A_Star path{"grid.txt"};
   path.printGrid();
   path.calculateShortest();
