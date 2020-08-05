f98f6042e474c9cc09246379e3218313123ce38d
randypalusz <ibanezguy998@gmail.com>
2020-08-05 13:45:43 -0400

    remove OpenGL references in CMakeLists.txt/main.cc

--- src/main.cc
+++ src/main.cc
@@ -3,59 +3,12 @@
 // https://github.com/JoeyDeVries/LearnOpenGL
 // https://learnopengl.com/
 
-// OpenGL includes must be in this order
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
 #include <chrono>
 #include <vector>
 
 #include "A_Star.hpp"
 
-void glfunctions();
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
-
-void processInput(GLFWwindow* window) {
-  if (glfwGetKey(window, GLFW_KEY_END) == GLFW_PRESS) {
-    glfwSetWindowShouldClose(window, true);
-  }
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-  glViewport(0, 0, width, height);
-}
-
-void glfunctions() {
-  glfwInit();
-  glfwWindowHint(GLFW_SAMPLES, 4);
-  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
-  GLFWwindow* window = glfwCreateWindow(800, 600, "Window", NULL, NULL);
-  if (!window) {
-    std::cout << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
-  }
-
-  glfwMakeContextCurrent(window);
-
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cout << "Failed to initialize GLAD" << std::endl;
-  }
-  glViewport(0, 0, 800, 600);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-  while (!glfwWindowShouldClose(window)) {
-    processInput(window);
-    glfwSwapBuffers(window);
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwPollEvents();
-  }
-  glfwTerminate();
-}
-
 int main(int argc, char** argv) {
-  glfunctions();
   // std::vector<std::vector<char>> grid{};
   // grid.push_back(
   //     std::vector<char>{'.', '#', '.', '.', '.', '.', '.', '#', '.', '.', '.', '.'});
@@ -75,5 +28,4 @@ int main(int argc, char** argv) {
   path.printGrid();
   path.calculateShortest();
   path.printGrid();
-  glfwTerminate();
 }
